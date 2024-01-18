56415fcd7e3619c55494a94dc648d78ae6732c78
Ananta-01 <anantasontakke143@gmail.com>
2024-01-18 23:17:27 +0530

    bg fg comp added

--- App.tsx
+++ App.tsx
@@ -1,74 +1,22 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+// App.js
 
 import React from 'react';
-import type { PropsWithChildren } from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import { SafeAreaView, StyleSheet, Text } from 'react-native';
+import Background from './assets/components/Background';
+import Foreground from './assets/components/Foreground';
 
-import {
-  Colors,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
+export default function App() {
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-      <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-
-
-          <Text>Hello</Text>
-
-      </View>
-      </ScrollView>
+    <SafeAreaView style={styles.container}>
+       <Background />
+      <Foreground/> 
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  container: {
+    flex: 1,
+    backgroundColor: '#265073',
   },
 });
-
-export default App;
---
+++ assets/components/Background.tsx
@@ -0,0 +1,36 @@
+// Background.js
+
+import React from 'react';
+import { SafeAreaView, View, Text, StyleSheet } from 'react-native';
+
+function Background() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.innerContainer}>
+        <Text style={styles.text}>
+          Docs...
+        </Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+   
+  },
+  innerContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#FBF9F1',
+    fontSize: 50,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
+
+export default Background;
