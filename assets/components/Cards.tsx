bff7c83e4c6040a5eaf0bb8bd2e5ec5b0632534d
Ananta-01 <anantasontakke143@gmail.com>
2024-01-20 01:13:24 +0530

    card styling half done

--- assets/components/Cards.tsx
+++ assets/components/Cards.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
 import { Dimensions, ScrollView, View } from 'react-native';
-import { Card, Text,Button } from 'react-native-paper';
+import { Card, Text, Button } from 'react-native-paper';
 import Icon from 'react-native-vector-icons/FontAwesome5';
 
 const subjects = [
-  { id: 1, name: 'Card 1' },
+  { id: 1, name: 'Card 1', footer: '4mb' },
   { id: 2, name: 'Card 2' },
-  { id: 3, name: 'Card 3' },
+  { id: 3, name: 'Card 3', footer: '4mb' },
   { id: 4, name: 'Card 4' },
 ];
 
@@ -22,23 +22,38 @@ const Cards = () => (
       justifyContent: 'center',
     }}>
       {subjects.map((subject, i) => (
-        <Card key={subject.id} style={{
+        <View key={subject.id} style={{
           marginTop: cardGap,
           marginLeft: i % 2 !== 0 ? cardGap : 0,
           width: cardWidth,
-          height: 180,
-          backgroundColor: 'white',
+          height: 150,
+          backgroundColor: 'rgba(0, 0, 0, 0.4);',
           borderRadius: 16,
           shadowOpacity: 0.2,
-          justifyContent: 'center',
-          alignItems: 'center',
+          position: 'relative',
         }}>
-         <Icon size={24} color="black" name="file-alt" />
-          <Card.Content>
-            <Text variant="titleLarge">Card title {subject.name}</Text>
-            <Text variant="bodyMedium">Card content</Text>
-          </Card.Content>
-        </Card>
+          <View style={{ margin: 8 }}>
+            <Icon size={24} color="white" name="file-alt" />
+            <Text variant="titleMedium" style={{ color: 'white', }} >Card title {subject.name}</Text>
+            <Text variant="labelSmall" style={{ color: 'white', }} >Lorem ipsum dolor sit amet, consectetur adipisicing.</Text>
+          </View>
+
+          {subject.footer ? <View style={{
+            position: 'absolute',
+            left: 0,
+            right: 0,
+            bottom: 0,
+            borderBottomLeftRadius: 16,
+            borderBottomRightRadius: 16,
+            backgroundColor: '#92C7CF',
+            alignItems: 'center'
+          }}>
+            <Text style={{ fontSize: 10 }}> {subject.footer} </Text>
+          </View> : ''}
+
+
+        </View>
+
       ))}
     </View>
   </>
